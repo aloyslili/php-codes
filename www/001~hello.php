0d81afbdc127cb500d9330ef497bfc2cd9fa2c1c
c22b_h1v74hfhcc <1702251811@qq.com>
2022-05-23 00:35:47 +0800

    初始化

---
+++ www/001~hello.php
@@ -0,0 +1,18 @@
+<?php
+
+echo 'Hello';
+
+echo "<br>";
+
+$a = 3;
+$b = 4;
+$c = $a + $b;
+echo $c . "<br />";
+
+echo "$a + $b = $c";
+
+for ($i = 0; $i < 100; $i++) {
+    echo "$i" . "<br>";
+}
+
+
---
+++ www/test-get.php
@@ -0,0 +1,7 @@
+<?php
+
+//解决跨域
+header('Access-Control-Allow-Origin: *');
+
+//echo "请求参数：$_GET";
+var_dump($_GET);
